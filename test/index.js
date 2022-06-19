30cfc681f308ee7df59d7d9924bc1fe17650f409
Maureen0007 <maureen.wanjiku@student.moringaschool.com>
2022-06-19 20:42:51 +0300

    complete assignment

--- test/index.js
+++ test/index.js
@@ -1,6 +1,5 @@
 const chai = require("chai");
 global.expect = chai.expect;
-const isPalindrome = require("../index");
 
 describe("isPalindrome", () => {
   it("returns true for 'abba'", () => {
@@ -19,3 +18,56 @@ describe("isPalindrome", () => {
     expect(isPalindrome("ab")).to.be.false;
   });
 });
+
+
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
+}
+
+function isPalindrome(word) {
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
